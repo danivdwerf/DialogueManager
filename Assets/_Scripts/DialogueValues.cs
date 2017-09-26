597f11befc56cdb968a029fc0b07758a442d7a2f
danivdwerf@gmail.com <danivdwerf@gmail.com>
2017-09-26 09:47:58 +0200

    added class to hold dialogue values

---
+++ Assets/_Scripts/DialogueValues.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+using System.Xml;
+using System.Collections.Generic;
+
+public struct Option_node
+{
+    private string option;
+    public string Option{get{return option;}}
+
+    private int response;
+    public int Response{get{return response;}}
+
+    public Option_node(string option, int response)
+    {
+        this.option = option;
+        this.response = response;
+    }
+};
+
+public struct Message_node
+{
+    private string message;
+    public string Message{get{return message;}}
+
+    private List<Option_node> options;
+    public List<Option_node> Options{get{return options;}}
+
+    public Message_node(string message)
+    {
+        this.message = message;
+        options = new List<Option_node>();
+    }
+
+    public void addOption(Option_node option)
+    {
+        options.Add(option);
+    }
+};
+
+public static class DialogueValues
+{
+    public static XmlDocument dialogueDocument = LoadXMLFile.loadFromURL("http://freetimedev.com/resources/projects/dialogue/dialogue.xml");
+}
--- Assets/_Scripts/NPC's/NPC.cs
+++ Assets/_Scripts/NPC's/NPC.cs
@@ -5,41 +5,6 @@ using System.Xml;
 using System.Linq;
 using System.Collections.Generic;
 
-public struct Option_node
-{
-    private string option;
-    public string Option{get{return option;}}
-
-    private int response;
-    public int Response{get{return response;}}
-
-    public Option_node(string option, int response)
-    {
-        this.option = option;
-        this.response = response;
-    }
-};
-    
-public struct Message_node
-{
-    private string message;
-    public string Message{get{return message;}}
-
-    private List<Option_node> options;
-    public List<Option_node> Options{get{return options;}}
-
-    public Message_node(string message)
-    {
-        this.message = message;
-        options = new List<Option_node>();
-    }
-
-    public void addOption(Option_node option)
-    {
-        options.Add(option);
-    }
-};
-
 [RequireComponent(typeof(NPCui))]
 public abstract class NPC : MonoBehaviour 
 {
@@ -55,8 +20,7 @@ public abstract class NPC : MonoBehaviour
     protected virtual void Start()
     {
         this.setReferences();
-//        this.loadDialogueFile(LoadXMLFile.loadFromResource("dialogue"));
-        this.loadDialogueFile(LoadXMLFile.loadFromURL("http://freetimedev.com/resources/projects/dialogue/dialogue.xml"));
+        this.loadDialogueFile(DialogueValues.dialogueDocument);
     }
 
     protected void setReferences()
